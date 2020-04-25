1f6ba1eb08c99965aa12526ac6cbe79c5c677471
石塚裕規 <ishizuka215@gmail.com>
2020-04-25 12:00:58 +0900

    update

--- udemy/src/class.ts
+++ udemy/src/class.ts
@@ -1,5 +1,5 @@
 class Person {
-    constructor(public name: string, private age: number ) {
+    constructor(public readonly name: string, private age: number ) {
     }
     incrementAge(){
         this.age++;
