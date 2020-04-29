6e2613d60141ca151c3d61fc991aa94a065b59df
zukaishi <ishizuka215@gmail.com>
2020-04-29 21:21:56 +0900

    update

--- udemy/dist/interface.js
+++ udemy/dist/interface.js
@@ -1,1 +1,6 @@
 "use strict";
+const human = {
+    name: 'Quil',
+    age: 38
+};
+let developer;
---
+++ udemy/src/interface.ts
@@ -0,0 +1,9 @@
+interface  Human  {
+    name: string;
+    age:number;
+}
+const human: Human = {
+    name: 'Quil',
+    age : 38
+}
+let developer: Human;
